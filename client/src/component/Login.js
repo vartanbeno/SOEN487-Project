9d64c2bd07ee2c4ab72dfbb5ebc80afa32ae9018
Giovanni <gprattico95@gmail.com>
2019-04-03 17:30:00 -0400

    add login form

--- client/src/component/Login.js
+++ client/src/component/Login.js
@@ -1,10 +1,53 @@
 import React, { Component } from 'react';
+import {
+    Col, Form,
+    FormGroup, Label, Input,
+    Button,
+    Container,
+  } from 'reactstrap';
 
 class Login extends Component {
 
     constructor(props){
         super(props);
 
+        this.state = {
+            username: '',
+            password: ''
+
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.postLogin = this.postLogin.bind(this);
+    }
+
+    handleChange(event){
+        const target = event.target;
+        const value = target.value;
+        const id = target.id;
+    
+        this.setState({[id]: value});
+    }
+
+    handleSubmit(event){
+
+        event.preventDefault();
+        
+        if(this.state.username.length<1){
+          alert('Please enter a username');
+        } else if(this.state.password.length<1){
+          alert('Please enter a password');
+        }else{
+          //this.postLogin();
+          console.log('Ready to submit')
+          this.postLogin();
+        }
+    }
+
+    postLogin(){
+        
+        console.log('insert nice query')
 
     }
 
@@ -12,8 +55,37 @@ class Login extends Component {
     render(){
 
         return (
+            <div>
+            <h1>Enter your credentials to Login</h1>
             
-                <h1>Login Form</h1>
+            <Form className="form" onSubmit={this.handleSubmit}>
+                <Col>
+                <br/>
+                <FormGroup>
+                    <Label>Username</Label>
+                    <Input
+                        type="username"
+                        name="username"
+                        id="username"
+                        onChange={this.handleChange}
+                />
+                <br/>
+                    <Label>Password</Label>
+                    <Input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder=""
+                        onChange={this.handleChange}
+                    />
+                <br/>
+                <Button style={{textAlign: 'right',alignSelf: 'stretch'}}type="submit" value="Submit">Submit</Button>
+              </FormGroup>
+            </Col>
+            <br/>
+            </Form>
+            </div>
+            
 
 
 
