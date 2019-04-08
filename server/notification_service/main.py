f6810714a008f6a9261fc5590f84b56eef57a389
Giovanni <gprattico95@gmail.com>
2019-04-07 21:06:59 -0400

    add token verification for notifications endpoints

--- server/notification_service/main.py
+++ server/notification_service/main.py
@@ -11,7 +11,7 @@ app.register_blueprint(nBp)
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return make_response(jsonify({"code": 404, "msg": "404: Not Found"}), 404)
+    return make_response(jsonify({"code": 404, "msg": "The endpoint you tried accessing does not exist"}), 404)
 
 
 @app.route('/')
