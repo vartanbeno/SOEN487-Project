97680039530bf0d95faa369e446880ff3748f150
OrangeUtang <Joe.dus.sen@gmail.com>
2019-04-24 00:40:07 -0400

    Delete manage.py

--- img_storage_service/ImgManager/manage.py
+++
@@ -1,7 +0,0 @@
-from main import app
-from ImgManager.models import db, Client, Album, Image
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return dict(app=app, db=db, Person=Client, Album=Album, Picture=Image)
