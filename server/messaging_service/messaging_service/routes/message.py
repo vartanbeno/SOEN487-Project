84ce613d4f5e1851f75577c7799cda7edc03322b
Vartan Benohanian <vartanbeno@gmail.com>
2019-04-27 12:58:15 -0400

    Notification message includes username
    Signed-off-by: Vartan Benohanian <vartanbeno@gmail.com>

--- server/messaging_service/messaging_service/routes/message.py
+++ server/messaging_service/messaging_service/routes/message.py
@@ -14,8 +14,13 @@ message_api = Blueprint('message_api', __name__)
 @message_api.route('', methods=['POST'])
 @jwt_required
 def create_message():
-    user_id = get_user_id_from_jwt()
     request_body = request.get_json()
+
+    token = get_token_from_authorization_header(request)
+    auth_header = get_authorization_header(token)
+
+    res = requests.get(f'http://localhost:5000/api/user/{get_user_id_from_jwt()}', headers=auth_header)
+    user = res.json()
 
     if 'conversation_id' not in request_body or 'text' not in request_body:
         return response('You must provide both conversation id and message text.', 400)
@@ -24,22 +29,20 @@ def create_message():
     text = request_body['text']
 
     conversation = Conversation.query.filter_by(id=conversation_id).first()
-    if conversation is None or (user_id != conversation.creator_id and user_id != conversation.participant_id):
+    if conversation is None or (user['id'] != conversation.creator_id and user['id'] != conversation.participant_id):
         return response('You are not part of this conversation.', 400)
 
-    receiver_id = conversation.creator_id if user_id == conversation.participant_id else conversation.participant_id
+    receiver_id = conversation.creator_id if user['id'] == conversation.participant_id else conversation.participant_id
 
-    message = Message(conversation_id=conversation.id, sender_id=user_id, text=text)
+    message = Message(conversation_id=conversation.id, sender_id=user['id'], text=text)
     db.session.add(message)
     db.session.commit()
 
-    token = get_token_from_authorization_header(request)
-    auth_header = get_authorization_header(token)
     requests.put(
         'http://localhost:8080/api/notifications',
         json={
             'receiverID': receiver_id,
-            'message': f'You have a new message from {user_id}.'
+            'message': f'You have a new message from {user["username"]}.'
         },
         headers=auth_header
     )
